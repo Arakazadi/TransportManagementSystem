67084a7e94c8586c4cd23b25f4f9599757747eed
Arakazadi <87508656+Arakazadi@users.noreply.github.com>
2021-08-24 19:48:30 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php 
 //  $connection=mysqli_connect("localhost","root","","heroku_5e97e1a347139a9");
   //Get Heroku ClearDB connection information
+    //Get Heroku ClearDB connection information
     $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-    $cleardb_server = $cleardb_url["us-cdbr-east-04.cleardb.com"];
-    $cleardb_username = $cleardb_url["b41d03081871e5"];
-    $cleardb_password = $cleardb_url["239fb33a"];
+    $cleardb_server = $cleardb_url["host"];
+    $cleardb_username = $cleardb_url["user"];
+    $cleardb_password = $cleardb_url["pass"];
     $cleardb_db = substr($cleardb_url["path"],1);
     $active_group = 'default';
     $query_builder = TRUE;
