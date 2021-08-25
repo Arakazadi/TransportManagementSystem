278ba022515fbed6b22b04dae7d3cd6ffc9bd1da
Arakazadi <87508656+Arakazadi@users.noreply.github.com>
2021-08-24 20:43:18 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php 
 //  $connection=mysqli_connect("localhost","root","","heroku_5e97e1a347139a9");
-$connection= mysqli_connect("localhost","root","","vehicle_management");
+//$connection= mysqli_connect("localhost","root","","vehicle_management");
 
 $connection= mysqli_connect("host","user","pass","heroku_5e97e1a347139a9");
   //Get Heroku ClearDB connection information
